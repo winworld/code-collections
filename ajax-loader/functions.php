3533d71ecd8ee99cfc2f9e8490a967e29baf8af2
Zaw <ztun.25@gmail.com>
2025-04-19 12:53:10 +0630

    ajax loader updated

--- ajax-loader/functions.php
+++ ajax-loader/functions.php
@@ -1,41 +1,24 @@
 <?php
-class AjaxLoader
-{
+class AjaxLoader {
     private static $instance = null;
-
-    public static function get_instance()
-    {
+   
+    public static function get_instance() {
         if (self::$instance === null) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
-    private function __construct()
-    {
-        add_filter('the_content', [$this, 'test_media']);
+    private function __construct() {        
         add_action('wp_ajax_load_more_items', [$this, 'ajax_load_more']);
         add_action('wp_ajax_nopriv_load_more_items', [$this, 'ajax_load_more']);
-    }
-
-    public function test_media($content)
-    {
-        if (is_page('stories') && isset($_GET['test'])) {
-            $content = $this->display_items(['post_type' => 'post', 'posts_per_page' => 14, 'category_name' => 'news-media,stories']);
-            return $content;
-        } elseif (is_page('publications') && isset($_GET['test'])) {
-            $content = $this->display_items(['post_type' => CF_POST_TYPE_PUBLICATION, 'posts_per_page' => 12]);
-            return $content;
-        }
-        return $content;
-    }
+    }   
 
     /**
      * Load content dynamically based on attributes.
      *   
      */
-    private function display_items($atts)
-    {
+    public function display_items($atts) {
         $atts = shortcode_atts([
             'post_type' => 'post',
             'posts_per_page' => 10,
@@ -66,8 +49,7 @@ class AjaxLoader
      * @param array $js_params JavaScript parameters for AJAX.
      * @return string Rendered HTML.
      */
-    private function get_items($atts, $js_params)
-    {
+    private function get_items($atts, $js_params) {
         $args = $this->build_query($atts);
         $query = new WP_Query($args);
 
@@ -88,9 +70,12 @@ class AjaxLoader
                 <?php $this->render($query, $template, $wrapper_class, $no_posts_message); ?>
             </div>
 
-            <?php if ($query->max_num_pages > 1): ?>
+            <?php if ($query->max_num_pages > 1) : ?>
                 <div class="text-center mb-4">
-                    <button id="load-more" data-page="2" data-type="<?php echo esc_attr($atts['post_type']); ?>"
+                    <button
+                        id="load-more"
+                        data-page="2"
+                        data-type="<?php echo esc_attr($atts['post_type']); ?>"
                         class="load-more-btn cta-button cta-button--secondary-dark-green cta-button--medium">
                         LOAD MORE <?php echo ($atts['post_type'] === CF_POST_TYPE_PUBLICATION ? 'PUBLICATIONS' : 'NEWS'); ?>
                     </button>
@@ -108,10 +93,7 @@ class AjaxLoader
                         nonce: rugbySettings.nonce,
                         action: '<?php echo ($js_params['action']); ?>',
                         post_type: '<?php echo esc_js($atts['post_type']); ?>',
-                        posts_per_page: <?php echo intval($atts['posts_per_page']); ?>,
-                        category: '',
-                        year: '',
-                        orderby: '',
+                        posts_per_page: <?php echo intval($atts['posts_per_page']); ?>,                      
                     },
                 });
             });
@@ -121,16 +103,14 @@ class AjaxLoader
         wp_reset_postdata();
         return ob_get_clean();
     }
-
+  
     /**
      * Render items dynamically based on the query.     
      */
-    private function render($query, $template, $wrapper_class = 'item-box', $no_posts_message = 'No posts found.')
-    {
-        if ($query->have_posts()):
+    private function render($query, $template, $wrapper_class = 'item-box', $no_posts_message = 'No posts found.') {
+        if ($query->have_posts()) :
             $counter = 0;
-            while ($query->have_posts()):
-                $query->the_post();
+            while ($query->have_posts()) : $query->the_post();
                 $counter++;
                 $class = $counter <= 6 ? 'col-lg-4 col-md-6 col-12' : 'col-lg-3 col-md-6 col-12';
                 ?>
@@ -139,7 +119,7 @@ class AjaxLoader
                 </div>
                 <?php
             endwhile;
-        else:
+        else :
             ?>
             <p><?php echo esc_html($no_posts_message); ?></p>
             <?php
@@ -149,8 +129,7 @@ class AjaxLoader
     /**
      * Handle AJAX requests for loading more items dynamically.
      */
-    public function ajax_load_more()
-    {
+    public function ajax_load_more() {
         if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'cfrugby_nonce')) {
             wp_send_json_error(['message' => 'Permission Denied']);
         }
@@ -171,7 +150,7 @@ class AjaxLoader
         $wrapper_class = $config['wrapper_class'];
         $no_posts_message = $config['no_posts_message'];
 
-        $args = $this->build_query($atts);
+        $args = $this->build_query($atts);        
         $query = new WP_Query($args);
 
         if ($query->have_posts()) {
@@ -200,12 +179,11 @@ class AjaxLoader
      * @param array $atts Attributes for the query.
      * @return array Query arguments.
      */
-    private function build_query($atts)
-    {
+    private function build_query($atts) {
         $args = [
             'post_type' => $atts['post_type'],
             'posts_per_page' => $atts['posts_per_page'] ?? 10,
-            'paged' => intval($atts['paged'] ?? 1),
+            'paged' => intval($atts['paged'] ?? 1),           
             'orderby' => (!empty($atts['orderby']) && $atts['orderby'] == 'date-asc' ? 'date' : 'date'),
             'order' => (!empty($atts['orderby']) && $atts['orderby'] == 'date-asc' ? 'ASC' : 'DESC'),
             's' => $atts['search'] ?? '',
@@ -214,7 +192,7 @@ class AjaxLoader
         ];
 
         $mappings = $this->get_query_mapping($atts);
-
+      
         // Add valid keys dynamically
         foreach (['category_name', 'year'] as $key) {
             if (!empty($mappings[$key])) {
@@ -227,7 +205,9 @@ class AjaxLoader
             foreach ($mappings['tax_query'] as $key => $taxonomy_config) {
                 if (!empty($_POST[$key])) {
                     $args['tax_query'][] = array_merge($taxonomy_config, [
-                        'terms' => is_array($_POST[$key]) ? array_map('sanitize_text_field', $_POST[$key]) : sanitize_text_field($_POST[$key]),
+                        'terms' => is_array($_POST[$key]) 
+                          ? array_map('sanitize_text_field', $_POST[$key]) 
+                          : sanitize_text_field($_POST[$key]),
                     ]);
                 }
             }
@@ -246,14 +226,13 @@ class AjaxLoader
 
         return $args;
     }
-
+   
     /**
      * Get query mappings for, unique keys, tax_query and meta_query.
      *
      * @return array
      */
-    private function get_query_mapping($atts)
-    {
+    private function get_query_mapping($atts) {
         $mappings = [
             'tax_query' => $this->get_taxonomies(),
             'meta_query' => $this->get_meta_queries(),
@@ -269,16 +248,14 @@ class AjaxLoader
         return $mappings;
     }
 
-    private function get_taxonomies()
-    {
+    private function get_taxonomies() {
         return [
             'pub_category' => $this->build_tax_query(CF_TAXO_PUB_CATEGORY),
-            'pub_year' => $this->build_tax_query(CF_TAXO_PUB_YEAR),
+            'pub_year' => $this->build_tax_query(CF_TAXO_PUB_YEAR),            
         ];
     }
 
-    private function build_tax_query($taxonomy, $field = 'term_id')
-    {
+    private function build_tax_query($taxonomy, $field = 'term_id') {
         return [
             'taxonomy' => $taxonomy,
             'field' => $field, // Dynamically set the field type
@@ -286,26 +263,23 @@ class AjaxLoader
         ];
     }
 
-    private function get_meta_queries()
-    {
+    private function get_meta_queries() {
         return [
-            'custom_meta_key' => $this->build_meta_query('custom_meta_key'),
+            'custom_meta_key' => $this->build_meta_query('custom_meta_key'),            
         ];
     }
 
-    private function build_meta_query($meta_key)
-    {
+    private function build_meta_query($meta_key) {
         return [
             'key' => $meta_key,
             'compare' => '=',
         ];
-    }
-
+    }  
+  
     /**
      * Get template, wrapper class, and no-posts message based on post type.    
      */
-    private function get_template_and_wrapper($post_type)
-    {
+    private function get_template_and_wrapper($post_type) {
         switch ($post_type) {
             case 'post':
                 return [
@@ -329,33 +303,32 @@ class AjaxLoader
                 ];
         }
     }
-
-    public static function get_years($post_type = 'post')
-    {
+  
+    public static function get_years($post_type = 'post') {      
         $years = [];
-
+        
         $args = [
-            'post_type' => $post_type,
-            'post_status' => 'publish',
-            'posts_per_page' => -1,
-            'fields' => 'ids',
+            'post_type'      => $post_type,
+            'post_status'    => 'publish',
+            'posts_per_page' => -1, 
+            'fields'         => 'ids', 
         ];
-
+        
         $posts = get_posts($args);
 
         foreach ($posts as $post_id) {
-            $year = get_the_date('Y', $post_id);
+            $year = get_the_date('Y', $post_id); 
             if (!in_array($year, $years)) {
-                $years[] = $year;
+                $years[] = $year; 
             }
         }
-
+        
         rsort($years);
 
         return $years;
     }
 }
 
-add_action('init', function () {
-    AjaxLoader::get_instance();
+add_action('init', function() {
+  AjaxLoader::get_instance();  
 });
